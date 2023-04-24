1cbb975b27c22b0446b63a042e243d5340c555ab
Anton Mokhovikov <ammokhov@amazon.com>
2023-04-24 15:16:18 -0700

    Added schema resolver (#11)

---
+++ src/rpdk/guard_rail/utils/schema_utils.py
@@ -0,0 +1,53 @@
+"""Module to handle schema manipulations."""
+from typing import Dict, Any, Set
+from jsonschema import RefResolver
+
+_PROPERTIES = "properties"
+_REF = "$ref"
+_ITEMS = "items"
+_PATTERN_PROPERTIES = "patternProperties"
+_ANY_OF = "anyOf"
+_ONE_OF = "oneOf"
+_ALL_OF = "allOf"
+
+
+def resolve_schema(schema: Dict):
+    """Resolving schema into a nested object.
+    Json schema allows recursive and chained
+    $refs, which is hard to analyze and get diff
+    between two schemas. This method resolves refs;
+    Args:
+        schema (Dict): _description_
+    Returns:
+        _type_: _description_
+    """
+    resolver = RefResolver.from_schema(schema)
+    def _internal_resolver(schema: Dict, resolver: Any, resolved_refs: Set):
+        if _PROPERTIES in schema:
+            for key, val in schema[_PROPERTIES].items():
+                schema[_PROPERTIES][key] = _internal_resolver(val, resolver, resolved_refs)
+        if _REF in schema:
+            reference_path = schema.pop(_REF, None)
+            if reference_path in resolved_refs:
+                return schema
+            resolved = resolver.resolve(reference_path)[1]
+            schema.update(resolved)
+            return _internal_resolver(schema, resolver, resolved_refs | {reference_path})
+        if _PATTERN_PROPERTIES in schema:
+            for key, val in schema[_PATTERN_PROPERTIES].items():
+                schema[_PATTERN_PROPERTIES][key] = _internal_resolver(val, resolver, resolved_refs)
+        if _ITEMS in schema:
+            schema[_ITEMS] = _internal_resolver(schema[_ITEMS], resolver, resolved_refs)
+        if _ALL_OF in schema:
+            for index, element in enumerate(schema[_ALL_OF]):
+                schema[_ALL_OF][index] = _internal_resolver(element, resolver, resolved_refs)
+        if _ANY_OF in schema:
+            for index, element in enumerate(schema[_ANY_OF]):
+                schema[_ANY_OF][index] = _internal_resolver(element, resolver, resolved_refs)
+        if _ONE_OF in schema:
+            for index, element in enumerate(schema[_ONE_OF]):
+                schema[_ONE_OF][index] = _internal_resolver(element, resolver, resolved_refs)
+        return schema
+    resolved_schema = _internal_resolver(schema, resolver, set())
+    resolved_schema.pop("definitions", None)
+    return resolved_schema
---
+++ tests/unit/utils/test_schema_utils.py
@@ -0,0 +1,30 @@
+"""unittest module to test schema utils"""
+import os
+from pathlib import Path
+import pytest
+from src.rpdk.guard_rail.utils.schema_utils import resolve_schema
+from src.rpdk.guard_rail.utils.arg_handler import collect_schemas
+
+@pytest.mark.parametrize(
+    "schema,result",
+    [
+        (
+            "data/schemas-for-testing/schema-with-chained-refs.json",
+            "data/schemas-for-verifying/schema-with-chained-refs.json"
+        )
+    ]
+)
+def test_resolve_schema(schema, result):
+    """Unit test to verify resolve_schema method"""
+    try:
+        collected_schemas_to_resolve = collect_schemas(schemas=[
+            "file:/"
+            + str(Path(os.path.dirname(os.path.realpath(__file__))).joinpath(schema))
+        ])
+        collected_schemas_to_verify = collect_schemas(schemas=[
+            "file:/"
+            + str(Path(os.path.dirname(os.path.realpath(__file__))).joinpath(result))
+        ])
+        assert resolve_schema(collected_schemas_to_resolve[0]) == collected_schemas_to_verify[0]
+    except IOError as e:
+        assert "No such file or directory: " in str(e)
