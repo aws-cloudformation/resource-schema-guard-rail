ef048ac863b35e3f86b510ed5ba2f2a9383e081f
Sai Gopal Athili <sgathili@amazon.com>
2023-04-25 13:45:54 -0700

    Fix format issues

--- src/rpdk/guard_rail/utils/schema_utils.py
+++ src/rpdk/guard_rail/utils/schema_utils.py
@@ -1,5 +1,6 @@
 """Module to handle schema manipulations."""
-from typing import Dict, Any, Set
+from typing import Any, Dict, Set
+
 from jsonschema import RefResolver
 
 _PROPERTIES = "properties"
@@ -22,32 +23,46 @@ def resolve_schema(schema: Dict):
         _type_: _description_
     """
     resolver = RefResolver.from_schema(schema)
+
     def _internal_resolver(schema: Dict, resolver: Any, resolved_refs: Set):
         if _PROPERTIES in schema:
             for key, val in schema[_PROPERTIES].items():
-                schema[_PROPERTIES][key] = _internal_resolver(val, resolver, resolved_refs)
+                schema[_PROPERTIES][key] = _internal_resolver(
+                    val, resolver, resolved_refs
+                )
         if _REF in schema:
             reference_path = schema.pop(_REF, None)
             if reference_path in resolved_refs:
                 return schema
             resolved = resolver.resolve(reference_path)[1]
             schema.update(resolved)
-            return _internal_resolver(schema, resolver, resolved_refs | {reference_path})
+            return _internal_resolver(
+                schema, resolver, resolved_refs | {reference_path}
+            )
         if _PATTERN_PROPERTIES in schema:
             for key, val in schema[_PATTERN_PROPERTIES].items():
-                schema[_PATTERN_PROPERTIES][key] = _internal_resolver(val, resolver, resolved_refs)
+                schema[_PATTERN_PROPERTIES][key] = _internal_resolver(
+                    val, resolver, resolved_refs
+                )
         if _ITEMS in schema:
             schema[_ITEMS] = _internal_resolver(schema[_ITEMS], resolver, resolved_refs)
         if _ALL_OF in schema:
             for index, element in enumerate(schema[_ALL_OF]):
-                schema[_ALL_OF][index] = _internal_resolver(element, resolver, resolved_refs)
+                schema[_ALL_OF][index] = _internal_resolver(
+                    element, resolver, resolved_refs
+                )
         if _ANY_OF in schema:
             for index, element in enumerate(schema[_ANY_OF]):
-                schema[_ANY_OF][index] = _internal_resolver(element, resolver, resolved_refs)
+                schema[_ANY_OF][index] = _internal_resolver(
+                    element, resolver, resolved_refs
+                )
         if _ONE_OF in schema:
             for index, element in enumerate(schema[_ONE_OF]):
-                schema[_ONE_OF][index] = _internal_resolver(element, resolver, resolved_refs)
+                schema[_ONE_OF][index] = _internal_resolver(
+                    element, resolver, resolved_refs
+                )
         return schema
+
     resolved_schema = _internal_resolver(schema, resolver, set())
     resolved_schema.pop("definitions", None)
     return resolved_schema
