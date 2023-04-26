de7edd8e1b84e97e920d51be5947d78a12a07508
Sai Gopal Athili <sgathili@amazon.com>
2023-04-25 21:22:53 -0700

    Statefull implementation

--- src/rpdk/guard_rail/core/stateful.py
+++ src/rpdk/guard_rail/core/stateful.py
@@ -1,35 +1,277 @@
 """Module to perform statefull schema diff.
 """
-# import re
-# from typing import Any, Collection, Dict, Iterable
-#
-# from deepdiff import DeepDiff
-#
-#
-# def json_diff(previous_json: Dict[str, Any], current_json: Dict[str, Any]):
-#     deep_diff = DeepDiff(previous_json, current_json, ignore_order=True)
-#     return _format_json_diff_(deep_diff.to_dict())
-#
-#
-# def _cast_root_to_path_(value: str):
-#     pattern = r"(?<=\[)([\s\S]+?)(?=\])"
-#     result = re.findall(pattern, value)
-#     return "/".join(
-#         [""] + [item[1:-1] if item[0] == item[-1] == "'" else item for item in result]
-#     )
-#
-#
-# def _format_json_diff_(iterable: Iterable, regex: str = r"root\["):
-#     if isinstance(iterable, dict):
-#         for key in list(iterable.keys()):
-#             new_key = key
-#             if re.match(regex, key):
-#                 new_key = _cast_root_to_path_(key)
-#             iterable[new_key] = _format_json_diff_(iterable.pop(key), regex)
-#     elif isinstance(iterable, Collection) and not isinstance(iterable, str):
-#         return [
-#             _cast_root_to_path_(item) if re.match(regex, item) else item
-#             for item in iterable
-#         ]
-#     else:
-#         return iterable
+import re
+from enum import auto
+from functools import partial
+from typing import Any, Dict, Iterable
+
+import strenum
+from deepdiff import DeepDiff
+from src.rpdk.guard_rail.utils.schema_utils import resolve_schema
+
+
+class METADIFF(strenum.LowercaseStrEnum):
+    ITERABLE_ITEM_ADDED = auto()
+    ITERABLE_ITEM_REMOVED = auto()
+    VALUES_CHANGED = auto()
+    DICTIONARY_ITEM_ADDED = auto()
+    DICTIONARY_ITEM_REMOVED = auto()
+
+
+class DIFFKEYS:
+    ADDED = "added"
+    REMOVED = "removed"
+    CHANGED = "changed"
+    OLD_VALUE = "old_value"
+    NEW_VALUE = "new_value"
+    PROPERTY = "property"
+
+
+PROPERTIES = "properties"
+cfn_list_constructs = {
+    "primaryIdentifier",
+    "readOnlyProperties",
+    "writeOnlyProperties",
+    "createOnlyProperties",
+    "additionalIdentifiers",
+}
+native_constructs = {
+    "type",
+    "description",
+    "enum",
+    "maximum",
+    "minimum",
+    "maxLength",
+    "minLength",
+    "pattern",
+    "maxItems",
+    "minItems",
+    "contains",
+    "items",
+    "additionalProperties",
+}
+
+
+def schema_diff(previous_json: Dict[str, Any], current_json: Dict[str, Any]):
+    """schema diff function to get formatted schema diff from deep diff"""
+    deep_diff = DeepDiff(
+        resolve_schema(previous_json),
+        resolve_schema(current_json),
+        ignore_order=True,
+        verbose_level=2,
+    )
+    return _translate_meta_diff(deep_diff.to_dict())
+
+
+def is_property(path_list):
+    """
+
+    Args:
+        path_list:
+
+    Returns:
+
+    """
+    return (
+        ((path_list[0] == PROPERTIES) and (path_list[-1] not in native_constructs))
+        if len(path_list) > 0
+        else False
+    )
+
+
+def is_cfn_construct(path_list):
+    """
+
+    Args:
+        path_list:
+
+    Returns:
+
+    """
+    return path_list[0] in cfn_list_constructs if len(path_list) > 0 else False
+
+
+def is_native_construct(path_list):
+    """
+
+    Args:
+        path_list:
+
+    Returns:
+
+    """
+    return path_list[-1] in native_constructs if len(path_list) > 0 else False
+
+
+def _get_path(path_list):
+    """
+
+    Args:
+        path_list:
+
+    Returns:
+
+    """
+    return "/".join([""] + path_list)
+
+
+def _cast_path(value: str):
+    """cast the path of the change to process constructs"""
+    pattern = r"(?<=\[)'?([\s\S]+?)'?(?=\])"
+    result = re.findall(pattern, value)
+    return result
+
+
+def _add_item(constructs_diff, result_key, change_key, result_value):
+    """_summary_
+    Args:
+        constructs_diff (_type_): _description_
+        result_key (_type_): _description_
+        change_key (_type_): _description_
+        result_value (_type_): _description_
+    """
+    if result_key not in constructs_diff:
+        constructs_diff[result_key] = {}
+    if change_key in constructs_diff[result_key]:
+        constructs_diff[result_key][change_key].append(result_value)
+    else:
+        constructs_diff[result_key][change_key] = [result_value]
+
+
+def _translate_iterable_change(
+    changed: DIFFKEYS, constructs_diff: Dict[str, Any], diff_value: Any
+):
+    """_summary_
+    Args:
+        constructs_diff (Dict[str, Any]): _description_
+        diff_value (Any): _description_
+        changed (DIFFKEYS): _description_
+    """
+
+    def __translate_iter_added_diff(constructs_diff, diff_value):
+        for key, value in diff_value.items():
+            path_list = _cast_path(key)
+            if is_cfn_construct(path_list):
+                _add_item(constructs_diff, path_list[0], DIFFKEYS.ADDED, value)
+
+    def __translate_iter_removed_diff(constructs_diff, diff_value):
+        for key, value in diff_value.items():
+            path_list = _cast_path(key)
+            if is_cfn_construct(path_list):
+                _add_item(constructs_diff, path_list[0], DIFFKEYS.REMOVED, value)
+
+    if changed == DIFFKEYS.ADDED:
+        __translate_iter_added_diff(constructs_diff, diff_value)
+        return
+    if changed == DIFFKEYS.REMOVED:
+        __translate_iter_removed_diff(constructs_diff, diff_value)
+        return
+
+    raise NotImplementedError("Invalid")
+
+
+def _translate_dict_change(
+    changed: DIFFKEYS, constructs_diff: Dict[str, Any], diff_value: Any
+):
+    """_summary_
+    Args:
+        constructs_diff (Dict[str, Any]): _description_
+        diff_value (Any): _description_
+        changed (DIFFKEYS): _description_
+    """
+
+    def __translate_dict_added_diff(constructs_diff, diff_value):
+        for key in diff_value:
+            path_list = _cast_path(key)
+            if is_property(path_list):
+                _add_item(
+                    constructs_diff, PROPERTIES, DIFFKEYS.ADDED, _get_path(path_list)
+                )
+            if is_native_construct(path_list):
+                _add_item(
+                    constructs_diff,
+                    path_list[-1],
+                    DIFFKEYS.ADDED,
+                    _get_path(path_list[:-1]),
+                )
+
+    def __translate_dict_removed_diff(constructs_diff, diff_value):
+        for key in diff_value:
+            path_list = _cast_path(key)
+            if is_property(path_list):
+                _add_item(
+                    constructs_diff, PROPERTIES, DIFFKEYS.REMOVED, _get_path(path_list)
+                )
+            if is_native_construct(path_list):
+                _add_item(
+                    constructs_diff,
+                    path_list[-1],
+                    DIFFKEYS.REMOVED,
+                    _get_path(path_list[:-1]),
+                )
+
+    if changed == DIFFKEYS.ADDED:
+        __translate_dict_added_diff(constructs_diff, diff_value)
+        return
+    if changed == DIFFKEYS.REMOVED:
+        __translate_dict_removed_diff(constructs_diff, diff_value)
+        return
+
+    raise NotImplementedError("Invalid")
+
+
+def _translate_values_changed_diff_(constructs_diff, diff_value):
+    """_summary_
+    Args:
+        constructs_diff (_type_): _description_
+        diff_value (_type_): _description_
+    """
+    for key, value in diff_value.items():
+        path_list = _cast_path(key)
+        if is_cfn_construct(path_list):
+            _add_item(
+                constructs_diff,
+                path_list[0],
+                DIFFKEYS.REMOVED,
+                value[DIFFKEYS.OLD_VALUE],
+            )
+            _add_item(
+                constructs_diff, path_list[0], DIFFKEYS.ADDED, value[DIFFKEYS.NEW_VALUE]
+            )
+        if is_native_construct(path_list):
+            _add_item(
+                constructs_diff,
+                path_list[-1],
+                DIFFKEYS.CHANGED,
+                {
+                    DIFFKEYS.PROPERTY: _get_path(path_list[:-1]),
+                    DIFFKEYS.OLD_VALUE: value[DIFFKEYS.OLD_VALUE],
+                    DIFFKEYS.NEW_VALUE: value[DIFFKEYS.NEW_VALUE],
+                },
+            )
+
+
+def _translate_meta_diff(iterable: Iterable):
+    """_summary_
+    Args:
+        iterable (Iterable): _description_
+    Returns:
+        _type_: _description_
+    """
+    diff_switcher = {
+        METADIFF.ITERABLE_ITEM_ADDED: partial(
+            _translate_iterable_change, DIFFKEYS.ADDED
+        ),
+        METADIFF.ITERABLE_ITEM_REMOVED: partial(
+            _translate_iterable_change, DIFFKEYS.REMOVED
+        ),
+        METADIFF.VALUES_CHANGED: _translate_values_changed_diff_,
+        METADIFF.DICTIONARY_ITEM_ADDED: partial(_translate_dict_change, DIFFKEYS.ADDED),
+        METADIFF.DICTIONARY_ITEM_REMOVED: partial(
+            _translate_dict_change, DIFFKEYS.REMOVED
+        ),
+    }
+    constructs_diff = {}
+    for diff_key, diff_value in iterable.items():
+        diff_switcher.get(diff_key, lambda: "Invalid")(constructs_diff, diff_value)
+    return constructs_diff
---
+++ tests/unit/core/test_stateful.py
@@ -0,0 +1,260 @@
+"""
+Unit test for stateful.py
+"""
+import pytest
+
+from src.rpdk.guard_rail.core.stateful import schema_diff
+
+
+@pytest.mark.parametrize(
+    "previous_schema, current_schema, expected_diff",
+    [
+        (
+            {"primaryIdentifier": ["bar"]},
+            {"primaryIdentifier": ["bar_changed", "bar_added"]},
+            {
+                "primaryIdentifier": {
+                    "added": ["bar_changed", "bar_added"],
+                    "removed": ["bar"],
+                }
+            },
+        ),
+        (
+            {
+                "properties": {
+                    "Description": {
+                        "description": "A description of the AWS KMS key.",
+                        "type": "string",
+                        "minLength": 0,
+                        "maxLength": 8192,
+                    }
+                },
+            },
+            {
+                "properties": {
+                    "Description": {
+                        "description": "A description of the AWS KMS key.",
+                        "minLength": 10,
+                        "maxLength": 4096,
+                    }
+                },
+            },
+            {
+                "type": {"removed": ["/properties/Description"]},
+                "minLength": {
+                    "changed": [
+                        {
+                            "property": "/properties/Description",
+                            "old_value": 0,
+                            "new_value": 10,
+                        }
+                    ]
+                },
+                "maxLength": {
+                    "changed": [
+                        {
+                            "property": "/properties/Description",
+                            "old_value": 8192,
+                            "new_value": 4096,
+                        }
+                    ]
+                },
+            },
+        ),
+    ],
+)
+def test_schema_diff(previous_schema, current_schema, expected_diff):
+    """
+
+    Args:
+        previous_schema:
+        current_schema:
+        expected_diff:
+    """
+    assert expected_diff == schema_diff(previous_schema, current_schema)
+
+
+@pytest.mark.parametrize(
+    "schema_variant1, schema_variant2, expected_diff, expected_diff_negative",
+    [
+        # Test Case #1: New Property
+        (
+            {"properties": {}},
+            {
+                "properties": {
+                    "NewProperty": {
+                        "type": "string",
+                        "description": "...",
+                        "minLength": 0,
+                        "maxLength": 129,
+                    }
+                }
+            },
+            {"properties": {"added": ["/properties/NewProperty"]}},
+            {"properties": {"removed": ["/properties/NewProperty"]}},
+        ),
+        # # Test Case #2: New Property with nested structure
+        # (
+        #         {
+        #             "properties": {}
+        #         },
+        #         {
+        #             "properties": {
+        #                 "Configurations": {
+        #                     'type': 'array',
+        #                     'items': {
+        #                         'type': 'object',
+        #                         'properties': {
+        #                             'Name': {
+        #                                 'type': 'string'
+        #                             },
+        #                             'Configurations': {  # recursive property
+        #                                 'type': 'array',
+        #                                 'items': {}  # ends at this level
+        #                             }
+        #                         }
+        #                     }
+        #                 }
+        #             }
+        #         },
+        #         {
+        #             "properties": {
+        #                 "added": [
+        #                     # we need to add root property
+        #                     # so as it's leafs as separate items
+        #                     "/properties/Configurations",
+        #                     "/properties/Configurations/*/Name",
+        #                     "/properties/Configurations/*/Configurations"
+        #                 ]
+        #             }
+        #         },
+        #         {
+        #             "properties": {
+        #                 "removed": [
+        #                     # we need to add root property
+        #                     # so as it's leafs as separate items
+        #                     "/properties/Configurations",
+        #                     "/properties/Configurations/*/Name",
+        #                     "/properties/Configurations/*/Configurations"
+        #                 ]
+        #             }
+        #         },
+        # ),
+        # # Test Case #3: New Nested Property
+        # (
+        #         {
+        #             "properties": {
+        #                 "Tags": {
+        #                     'type': 'array',
+        #                     'items': {
+        #                         'type': 'object',
+        #                         'properties': {
+        #                             'Key': {'type': 'string'}
+        #                         }
+        #                     }
+        #                 }
+        #             }
+        #         },
+        #         {
+        #             "properties": {
+        #                 "Tags": {
+        #                     'type': 'array',
+        #                     'items': {
+        #                         'type': 'object',
+        #                         'properties': {
+        #                             'Key': {'type': 'string'},
+        #                             'Value': {'type': 'string'}
+        #                         }
+        #                     }
+        #                 }
+        #             }
+        #         },
+        #         {
+        #             "properties": {
+        #                 "added": [
+        #                     "/properties/Tags/*/Value",
+        #                 ]
+        #             }
+        #         },
+        #         {
+        #             "properties": {
+        #                 "removed": [
+        #                     "/properties/Tags/*/Value",
+        #                 ]
+        #             }
+        #         },
+        # ),
+        # # Test Case #4: New Property with nested combiner (We might shelf this for now)
+        # (
+        #         {
+        #             "properties": {
+        #                 "Tags": {
+        #                     'type': 'array',
+        #                     'items': {
+        #                         'type': 'object',
+        #                         'properties': {
+        #                             'Key': {'type': 'string'},
+        #                             'Value': {'type': 'string'}
+        #                         }
+        #                     }
+        #                 }
+        #             }
+        #         },
+        #         {
+        #             "properties": {
+        #                 "Tags": {
+        #                     'type': 'array',
+        #                     'items': {
+        #                         'anyOf': [
+        #                             {
+        #                                 'type': 'object',
+        #                                 'properties': {
+        #                                     'Key': {'type': 'string'},
+        #                                     'Value': {'type': 'string'}
+        #                                 }
+        #                             },
+        #                             {
+        #                                 'type': 'object',
+        #                                 'properties': {
+        #                                     'TagKey': {'type': 'string'},
+        #                                     'TagValue': {'type': 'string'}
+        #                                 }
+        #                             }
+        #                         ]
+        #
+        #                     }
+        #                 }
+        #             }
+        #         },
+        #         {
+        #             "properties": {
+        #                 "added": [
+        #                     "/properties/Tags/*/TagKey",
+        #                     "/properties/Tags/*/TagValue",
+        #                 ]
+        #             }
+        #         },
+        #         {
+        #             "properties": {
+        #                 "removed": [
+        #                     "/properties/Tags/*/TagKey",
+        #                     "/properties/Tags/*/TagValue",
+        #                 ]
+        #             }
+        #         },
+        # ),
+    ],
+)
+def test_schema_diff_complex_property_mutations(
+    schema_variant1, schema_variant2, expected_diff, expected_diff_negative
+):
+    """
+
+    Args:
+        schema_variant1:
+        schema_variant2:
+        expected_diff:
+        expected_diff_negative:
+    """
+    assert expected_diff == schema_diff(schema_variant1, schema_variant2)
+    assert expected_diff_negative == schema_diff(schema_variant2, schema_variant1)
