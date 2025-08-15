b75f9a39f22099218db0bb11ddf21b4fe6b800f7
Michael Zhao <zhaomicx@amazon.com>
2025-08-15 16:35:24 -0700

    fix formatting errors

--- tests/unit/core/test_data_types_read_only.py
+++ tests/unit/core/test_data_types_read_only.py
@@ -7,11 +7,7 @@ from rpdk.guard_rail.core.data_types import Stateful, Stateless
 
 def test_stateless_with_read_only_flag():
     """Test Stateless data type with is_read_only flag"""
-    payload = Stateless(
-        schemas=[{"foo": "bar"}],
-        rules=[],
-        is_read_only=True
-    )
+    payload = Stateless(schemas=[{"foo": "bar"}], rules=[], is_read_only=True)
     assert payload.is_read_only is True
     assert payload.schemas == [{"foo": "bar"}]
     assert payload.rules == []
@@ -19,10 +15,7 @@ def test_stateless_with_read_only_flag():
 
 def test_stateless_default_read_only_flag():
     """Test Stateless data type with default is_read_only flag"""
-    payload = Stateless(
-        schemas=[{"foo": "bar"}],
-        rules=[]
-    )
+    payload = Stateless(schemas=[{"foo": "bar"}], rules=[])
     assert payload.is_read_only is False
 
 
@@ -32,7 +25,7 @@ def test_stateful_with_read_only_flag():
         current_schema={"foo": "bar"},
         previous_schema={"foo": "baz"},
         rules=[],
-        is_read_only=True
+        is_read_only=True,
     )
     assert payload.is_read_only is True
     assert payload.current_schema == {"foo": "bar"}
@@ -42,8 +35,6 @@ def test_stateful_with_read_only_flag():
 def test_stateful_default_read_only_flag():
     """Test Stateful data type with default is_read_only flag"""
     payload = Stateful(
-        current_schema={"foo": "bar"},
-        previous_schema={"foo": "baz"},
-        rules=[]
+        current_schema={"foo": "bar"}, previous_schema={"foo": "baz"}, rules=[]
     )
     assert payload.is_read_only is False
